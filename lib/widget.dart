69c786d09d23ea3e1181981446ed53c1215e6633
XRichardroidX <oguzieibehrichard@gmail.com>
2022-12-26 10:13:29 +0100

    Add-all

---
+++ lib/widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ItemX extends StatelessWidget {
+  const ItemX({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+            margin: EdgeInsets.fromLTRB(100, 0, 0, 90),
+            child: Text(
+              '',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.red,
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(70)
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+            margin: EdgeInsets.fromLTRB(0, 100, 10, 0),
+            child: Text(
+              'Animation 101',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.red,
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(70)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
